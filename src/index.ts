f97077fea705138e1edf2244874eebc155f0ddc8
Luka <lukabrkovic@proton.me>
2024-03-17 18:43:49 +0100

    feat: websockets

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
+import { log } from '~/lib/logger';
 import { db } from '~/lib/db';
 import { users } from '~/lib/schema';
 import { authRoutes } from '~/module/auth/auth.route';
 import { userRoutes } from './module/user/user.route';
+import { createBunWebSocket } from 'hono/bun';
+
 const app = new Hono().basePath('/api/v1');
+const { upgradeWebSocket, websocket } = createBunWebSocket();
 
 // this logger logs every request
 app.use('*', logger());
+app.use(
+  '*',
+  cors({
+    origin: 'http://localhost:3000',
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }),
+);
 
 // routes
 app.route('/auth', authRoutes);
@@ -23,8 +35,28 @@ app.get('/test-user', async (c) => {
   return c.json({ user });
 });
 
-// this is how we expose port and add api methods
-export default {
-  port: 8080,
+// terrible code, but works for now
+app.get(
+  '/ws',
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send(event.data + ' from server');
+      },
+      onOpen: (ws) => {
+        console.log('Client connected');
+      },
+      onError: (err) => {
+        console.error('Error:', err);
+      },
+    };
+  }),
+);
+
+log.info('Server is running on port 8080');
+Bun.serve({
   fetch: app.fetch,
-};
+  websocket,
+  port: 8080,
+});
